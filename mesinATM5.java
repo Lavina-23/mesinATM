eaa89864e20c2a0b04e77bb0375e49c7f6a117a1
Lavina-23 <lavinakohai3227@gmail.com>
2023-09-29 16:42:56 +0700

    login & menu

--- mesinATM5.java
+++ mesinATM5.java
@@ -1,9 +1,41 @@
+import java.util.Scanner;
+
 /**
  * mesinATM5
  */
 public class mesinATM5 {
 
     public static void main(String[] args) {
-        System.out.println("Hi Guys !");
+        Scanner sc = new Scanner(System.in);
+
+        int pin, menu, jmlSaldo, jmlTarik, jmlTF, noRek;
+        double bunga = 0.2;
+
+        System.out.print("Masukkan PIN anda : ");
+        pin = sc.nextInt();
+        
+        if (pin == 123) {
+            System.out.println("Silakan Pilih Menu Dibawah Ini");
+            System.out.println("1. Tarik Tunai \n2. Transfer \n3. Cek Saldo");
+            System.out.println("Ketik 1, 2 atau 3");
+            menu = sc.nextInt();
+            sc.close();
+            switch (menu) {
+                case 1:
+                    // Tarik Tunai dikerjakan disini
+                    break;
+                case 2:
+                    // Transfer kerjakan disini
+                    break;
+                case 3:
+                    // Cek Saldo kerjakan disini
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            System.out.println("PIN anda salah !");
+        }
+
     }
 }
