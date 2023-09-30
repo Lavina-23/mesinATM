e3ed3fa34ea56e25d11b5b9edef4a1376a605719
Alyssa Tifara Yuwono <tissaalyssa@gmail.com>
2023-09-30 19:52:51 +0700

    CekSaldo_Kelompok5_1BSIB_Alyssa

--- mesinATM5.java
+++ mesinATM5.java
@@ -1,41 +1,26 @@
 import java.util.Scanner;
 
+import javax.swing.text.Style;
 /**
  * mesinATM5
  */
 public class mesinATM5 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int pin, menu, jmlSaldo, jmlTarik, jmlTF, noRek;
-        double bunga = 0.2;
-
-        System.out.print("Masukkan PIN anda : ");
-        pin = sc.nextInt();
-        
-        if (pin == 123) {
-            System.out.println("Silakan Pilih Menu Dibawah Ini");
-            System.out.println("1. Tarik Tunai \n2. Transfer \n3. Cek Saldo");
-            System.out.println("Ketik 1, 2 atau 3");
-            menu = sc.nextInt();
-            sc.close();
-            switch (menu) {
-                case 1:
-                    // Tarik Tunai dikerjakan disini
-                    break;
-                case 2:
-                    // Transfer kerjakan disini
-                    break;
-                case 3:
-                    // Cek Saldo kerjakan disini
-                    break;
-                default:
-                    break;
-            }
-        } else {
-            System.out.println("PIN anda salah !");
+        Scanner input = new Scanner(System.in);
+        int NoRek, jmlTF, konfirmasi;
+        System.out.println("Masukkan nominal uang yang ingin ditranfer");
+        jmlTF = input.nextInt();
+        System.out.println("Masukkan nomor rekening yang dituju");
+        NoRek = input.nextInt();
+        System.out.println("Apakah anda yakin? (Tekan 1 = yakin dan klik 2 = tidak yakin)");
+        konfirmasi = input.nextInt();
+        if (konfirmasi == 1) {
+            System.out.println("Yakin");
         }
-
-    }
-}
+         else{
+            System.out.println("Transaksi tidak valid");
+         }
+            System.out.println("Transaksi anda berhasil dilakukan");
+        }
+        }
