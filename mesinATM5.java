9ebb44c7a6db6ee604e3a96518fffa955e7e5eb3
Alyssa Tifara Yuwono <tissaalyssa@gmail.com>
2023-10-28 14:25:43 +0700

    nyoba

--- mesinATM5.java
+++ mesinATM5.java
@@ -7,7 +7,7 @@ public class mesinATM5 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
         int pin, menu, jmlSaldo, jmlTarik, jmlTF, noRek, lama_menabung, sisaSaldo;
         double bunga = 0.2, jml_tabungan_akhir;
         String konfirmasi = "ya";
@@ -80,12 +80,43 @@ public class mesinATM5 {
                     } 
                 } while (true);
 
+                
                     case 2 :
+                    // Tranfer
+
                         do {
                         System.out.print("\nMasukkan nomor rekening tujuan : ");
                     noRek = sc.nextInt();
                     
+                    // Array
+                    int [] Transfer = {100000, 500000,1000000,5000000};
+                    int elemen, min;
 
+                    int [] Tranfer = new int[elemen];
+                    for (int i = 0; i <Transfer.length ; i++){
+                        System.out.println(Transfer[i]);
+                        Tranfer[i] = sc.nextInt();
+
+                    }
+
+                    int max = Transfer[3], main = Transfer[3], sum = Transfer[3];
+                    for (int i = 0; i<Transfer.length; i++){
+                        if (Transfer[3] > max){
+                            max = Transfer[3];
+
+                        }
+                        if (Transfer[3] < min){
+                            min = Transfer[3];
+                        }
+                        sum += Transfer[3];
+                    }
+
+                    double average = (double) sum / elemen;
+                    System.out.print("Nilai tertinggi : "+max);
+                    System.out.print("Nilai terendah :"+min);
+                    System.out.println("Nilai rata-rata: "+average);
+
+                    // Tranfer biasa
                     System.out.println("\nSilahkan Plih Menu Dibawah ini");
                     System.out.println("\n1. 100000 \n2. 500000 \n3. 1000000 \n4. 5000000");
                     System.out.println("Ketik 1, 2, 3 atau 4 ");
